7a1e160561754841d739825f9cf513bce8048573
A-NB <bel-an510@yandex.ru>
2022-01-16 19:48:57 +0300

    create README

--- p_library/views.py
+++ p_library/views.py
@@ -1,38 +1,40 @@
 from django.shortcuts import render
 from p_library.models import Book, PublishingHouse
-from django.http import HttpResponse
-from django.template import loader
+# from django.http import HttpResponse
+# from django.template import loader
 from django.shortcuts import redirect
+from django.contrib.auth.decorators import permission_required
+
 
 def index(request):
-    template = loader.get_template('index.html')    
+    # template = loader.get_template('index.html')    
     welcome_dict = {
         "welcome_phrase": "Добро пожаловать в мою библиотеку!",
     }
-    return HttpResponse(template.render(welcome_dict, request,)) 
-
-def make_range(request):
-    range_ = range(1, 101)
-    return HttpResponse(range_)
+    # return HttpResponse(template.render(welcome_dict, request,)) 
+    return render(request, 'index.html', context=welcome_dict,)     
 
 def books(request):
-    template = loader.get_template('books.html')
+    # template = loader.get_template('books.html')
     books = Book.objects.all()    
     biblio_data = {
+        "title": "Книги",        
         "books": books,
     }
-    return HttpResponse(template.render(biblio_data, request,)) 
+    # return HttpResponse(template.render(biblio_data, request,)) 
+    return render(request, 'books.html', context=biblio_data,)    
 
 def publishers(request):
-    template = loader.get_template('publishers.html')
+    # template = loader.get_template('publishers.html')
     pub_houses = PublishingHouse.objects.all()
     books_counter = Book.objects.count()
     pub_houses_data = {
-        "title": "Книжные издательства:",
+        "title": "Книжные издательства",
         "pub_houses": pub_houses,
         "books_counter": books_counter,
     }
-    return HttpResponse(template.render(pub_houses_data, request,)) 
+    # return HttpResponse(template.render(pub_houses_data, request,)) 
+    return render(request, 'publishers.html', context=pub_houses_data,)     
 
 def copy_count_change(request, action):
     if request.method == 'POST':
@@ -56,7 +58,7 @@ def copy_count_change(request, action):
         return redirect('/books/')
 
 def book_increment(request):
-    return(copy_count_change(request, '+'))
+    return copy_count_change(request, '+')
 
 def book_decrement(request):
-    return(copy_count_change(request, '-'))
+    return copy_count_change(request, '-')
